ea66e12ae1a591d2a6d0e4893441e6633041f819
Matthew Hiles <matthew.hiles@gmail.com>
2024-11-12 13:56:52 -0500

    add bounds checking to Blur()

--- xform/nondestructive.go
+++ xform/nondestructive.go
@@ -175,6 +175,9 @@ func (b *blur) At(x, y int) color.Color {
 	var i uint32
 	for sx := -1; sx < 2; sx++ {
 		for sy := -1; sy < 2; sy++ {
+			if sx < b.Image.Bounds().Min.X || sx > b.Image.Bounds().Max.X || sy < b.Image.Bounds().Min.Y || sy > b.Image.Bounds().Max.Y {
+				continue
+			}
 			r, g, b, a := b.Image.At(x+sx, y+sy).RGBA()
 			if a == 0 {
 				continue
